73782430c0a624211f5f1d88421f6ac4ae1ea725
Harikesh9511 <harikeshmishra9511@gmail.com>
2025-08-26 12:23:41 +0530

    production ready app

--- backend/index.js
+++ backend/index.js
@@ -7,15 +7,20 @@ import messageRoute from "./routes/messageRoute.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app,server } from "./socket/socket.js";
+import path from "path"
 dotenv.config({});
 
  
 const PORT = process.env.PORT || 5000;
 
+
+const _dirname = path.resolve();
 // middleware
 app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
+
+
 const corsOption={
     origin:'http://localhost:3000',
     credentials:true
@@ -23,9 +28,16 @@ const corsOption={
 app.use(cors(corsOption)); 
 
 
+
+
 // routes
 app.use("/api/v1/user",userRoute); 
 app.use("/api/v1/message",messageRoute);
+
+app.use(express.static(path.join(_dirname,"/frontend/build")))
+app.get('*', (_,res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "build", "index.html"));
+})
  
 
 server.listen(PORT, ()=>{
